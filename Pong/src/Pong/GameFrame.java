69f0c91599ed88d60e294fa6cc2460e28c2b24b6
Prathepa R <145280088+Pratheparampriya@users.noreply.github.com>
2024-07-03 18:59:15 +0530

    Add files via upload

---
+++ Pong/src/Pong/GameFrame.java
@@ -0,0 +1,26 @@
+package Pong;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * 
+ */
+public class GameFrame extends JFrame {
+	GamePanel panel;
+	GameFrame(){
+		panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Pong game");
+		this.setResizable(false);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+	}
+}
+
+
---
+++ Pong/src/Pong/PongGame.java
@@ -0,0 +1,14 @@
+package Pong;
+import java.util.*;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+public class PongGame {
+
+	public static void main(String[] args) {
+	GameFrame frame = new GameFrame();
+	
+
+	}
+
+}
